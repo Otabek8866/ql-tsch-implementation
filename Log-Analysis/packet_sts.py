fbb998c740a1dc315cb83eea0209c35c3a54cf72
Otabek8866 <sobirovotabek1995@gmail.com>
2022-04-04 21:22:07 +0200

    packet delays calculation finished

--- Log-Analysis/packet_sts.py
+++ Log-Analysis/packet_sts.py
@@ -1,79 +1,70 @@
-INPUT_FILE = "loglistener.txt"
-# number of nodes that have been simulated
-MAX_NODE_NUMBER = 0
-# delays for each node udp transmissions -> 2D matrix
+INPUT_FILE = "loglistener_example.txt"
+
 delays = {}
-node_udp_com = {}
-sent_and_received = {}
+pkt_sent = {}
+pkt_received = {}
 
 
 def main():
-    global MAX_NODE_NUMBER
     with open(INPUT_FILE, "r") as f:
         for line in f:
             if not (("INFO: App" in line) and ("Sent_to" in line or "Received_from" in line)):
                 continue
-            fields = line.split()
-            id = int(fields[1][3:])
-            if id == 1:
-                id = int(fields[6])
-            if id in node_udp_com:
-                node_udp_com[id].append(fields)
-            else:
-                node_udp_com[id] = [fields]
-                MAX_NODE_NUMBER += 1
-
-    for i in range(2, MAX_NODE_NUMBER + 2):
-        sent_and_received[i] = {'sent': [], 'delivered': []}
-        delays[i] = []
-        node_udp_com[i].sort(key=lambda x: (int(x[0]), int(x[-1])))
-        j = 0
-        while j < len(node_udp_com[i])-1:
-            if node_udp_com[i][j+1][5] == "Received_from":
-                if node_udp_com[i][j][-1] == node_udp_com[i][j+1][-1]:
-                    delays[i].append(
-                        int(node_udp_com[i][j+1][0]) - int(node_udp_com[i][j][0]))
-                    sent_and_received[i]['sent'].append(
-                        int(node_udp_com[i][j][-1]))
-                    sent_and_received[i]['delivered'].append(
-                        int(node_udp_com[i][j+1][-1]))
-                    j += 2
+            if "Sent_to" in line:
+                fields = line.split()
+                node_id = int(fields[1][3:])
+                if node_id in pkt_sent:
+                    pkt_sent[node_id][int(fields[-1])] = float(fields[0])
                 else:
-                    sent_and_received[i]['sent'].append(
-                        int(node_udp_com[i][j][-1]))
-                    j += 1
+                    pkt_sent[node_id] = {}
+                    pkt_sent[node_id][int(fields[-1])] = float(fields[0])
+            elif "Received_from" in line:
+                fields = line.split()
+                node_id = int(fields[6])
+                if node_id in pkt_received:
+                    pkt_received[node_id][int(fields[-1])] = float(fields[0])
+                else:
+                    pkt_received[node_id] = {}
+                    pkt_received[node_id][int(fields[-1])] = float(fields[0])
             else:
-                sent_and_received[i]['sent'].append(
-                    int(node_udp_com[i][j][-1]))
-                j += 1
+                print("Error: line not recognized")
 
-    total_average_delay = 0
+    for node_id in pkt_sent.keys():
+        if node_id in pkt_received:
+            pkt_id_list = sorted(set(pkt_sent[node_id].keys()) & set(
+                pkt_received[node_id].keys()))
+            not_received = sorted(
+                set(pkt_sent[node_id].keys()) - set(pkt_received[node_id].keys()))
+            pdr = float(len(pkt_received[node_id]))/len(pkt_sent[node_id])*100
+            dels = []
+            for pkt_id in pkt_id_list:
+                dels.append(pkt_received[node_id]
+                            [pkt_id] - pkt_sent[node_id][pkt_id])
+            delays[node_id] = (dels, not_received, pdr)
+
+        else:
+            delays[node_id] = ([], sorted(pkt_sent[node_id].keys()), 0.0)
+
+    total_time = 0
     counter = 0
-    for i in range(2, MAX_NODE_NUMBER + 2):
-        total_average_delay += sum(delays[i])
-        counter += len(delays[i])
-
-    total_sent = 0
-    total_delivered = 0
-    for i in range(2, MAX_NODE_NUMBER + 2):
-        total_sent += len(sent_and_received[i]['sent'])
-        total_delivered += len(sent_and_received[i]['delivered'])
+    average_pdr = 0
+    for i in delays.keys():
+        total_time += sum(delays[i][0])
+        counter += len(delays[i][0])
+        average_pdr += delays[i][2]
+    average_pdr = average_pdr/len(delays.keys())
 
     print("\n*********** Statistics for Network *************")
-    print("Average Delay: {:.2f} ms".format(total_average_delay/counter))
-    print("Packet Delivery Ratio: {:.2f}%".format(
-        (total_delivered/total_sent)*100))
+    print("Average Delay: {:.2f} ms".format(total_time/counter))
+    print("Packet Delivery Ratio: {:.2f}%".format(average_pdr))
 
     print("\n********* Average Delays for each Node *********")
-    for i in range(2, MAX_NODE_NUMBER + 2):
-        print("Node {}: {:.2f} ms".format(i, sum(delays[i])/len(delays[i])))
-
-    print("\n********* Packet Delivery Ratio *********")
-    for i in range(2, MAX_NODE_NUMBER + 2):
-        print("Node {}: {:.2f}".format(i, (len(
-            sent_and_received[i]['delivered'])/len(sent_and_received[i]['sent']))*100), end='\t')
-        print("Undeliivered Packets: {}, Packet indexes: {}".format(
-            len(sent_and_received[i]['sent']) - len(sent_and_received[i]['delivered']), list(set(sent_and_received[i]['sent']) - set(sent_and_received[i]['delivered']))))
+    for i in sorted(delays.keys()):
+        print("Node {}: {:.2f} ms".format(
+            i, sum(delays[i][0])/len(delays[i][0])))
+        print("Packet IDs not received: {}".format(delays[i][1]))
+        print("Packet Delivery Ratio: {:.2f}%".format(delays[i][2]))
+        print('-' * 50)
 
 
 if __name__ == "__main__":
--- test.c
+++ test.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
-    unsigned char ch[2];
-    unsigned int i = 1;
-    ch[0] = i & 0xff;
-    ch[1] = (i >> 8) & 0xff;
-    printf("%d %d\n", ch[0], ch[1]);
-    printf("%d\n", i);
-
-    unsigned int n;
-    n = ch[1] & 0xff;
-    n = n << 8;
-    n += ch[0];
-    printf("%d\n", n);
-}
+char a[10];
 
 // func to convert int to char array
-void int_to_char(int i, char *ch)
+void *foo()
 {
-    ch[0] = i & 0xff;
-    ch[1] = (i >> 8) & 0xff;
+    a[0] = 'a';
+    a[1] = 'b';
+    return a;
 }
 
-// convert char array to int
-int char_to_int(char *ch)
+int main()
 {
-    int n;
-    n = ch[1] & 0xff;
-    n += ch[0] << 8;
-    return n;
+    char *s = foo();
+    printf("%c\n", s[0]);
+    printf("%c\n", s[1]);
+    printf("%d\n", s[0] + s[1]);
+    return 0;
 }
